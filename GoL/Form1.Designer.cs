e529be0df29cbf2a768a73a1e35263729131451d
hayden <haydenshore@gmail.com>
2022-02-11 23:11:51 +0000

    fixed ui

--- GoL/Form1.Designer.cs
+++ GoL/Form1.Designer.cs
@@ -48,22 +48,28 @@ namespace GoL
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(3, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(20);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(3793, 1819);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.Size = new System.Drawing.Size(1043, 551);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // btnClickThis
             // 
-            this.btnClickThis.Location = new System.Drawing.Point(12, 1837);
+            this.btnClickThis.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnClickThis.Location = new System.Drawing.Point(3, 559);
+            this.btnClickThis.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnClickThis.Name = "btnClickThis";
-            this.btnClickThis.Size = new System.Drawing.Size(271, 115);
+            this.btnClickThis.Size = new System.Drawing.Size(136, 60);
             this.btnClickThis.TabIndex = 1;
             this.btnClickThis.Text = "Random Noise";
             this.btnClickThis.UseVisualStyleBackColor = true;
@@ -71,18 +77,22 @@ namespace GoL
             // 
             // lblHelloWorld
             // 
+            this.lblHelloWorld.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.lblHelloWorld.AutoSize = true;
-            this.lblHelloWorld.Location = new System.Drawing.Point(339, 1870);
+            this.lblHelloWorld.Location = new System.Drawing.Point(148, 559);
+            this.lblHelloWorld.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblHelloWorld.Name = "lblHelloWorld";
-            this.lblHelloWorld.Size = new System.Drawing.Size(70, 25);
+            this.lblHelloWorld.Size = new System.Drawing.Size(35, 13);
             this.lblHelloWorld.TabIndex = 2;
             this.lblHelloWorld.Text = "label1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1720, 1848);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(756, 559);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(279, 115);
+            this.button1.Size = new System.Drawing.Size(140, 60);
             this.button1.TabIndex = 3;
             this.button1.Text = "Play";
             this.button1.UseVisualStyleBackColor = true;
@@ -94,18 +104,22 @@ namespace GoL
             // 
             // speedBar
             // 
-            this.speedBar.Location = new System.Drawing.Point(1158, 1863);
+            this.speedBar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.speedBar.Location = new System.Drawing.Point(434, 561);
+            this.speedBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.speedBar.Name = "speedBar";
-            this.speedBar.Size = new System.Drawing.Size(482, 90);
+            this.speedBar.Size = new System.Drawing.Size(241, 45);
             this.speedBar.TabIndex = 4;
             this.speedBar.Scroll += new System.EventHandler(this.speedBar_Scroll);
             // 
             // StopButton
             // 
+            this.StopButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.StopButton.Enabled = false;
-            this.StopButton.Location = new System.Drawing.Point(2019, 1848);
+            this.StopButton.Location = new System.Drawing.Point(900, 559);
+            this.StopButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.StopButton.Name = "StopButton";
-            this.StopButton.Size = new System.Drawing.Size(279, 115);
+            this.StopButton.Size = new System.Drawing.Size(140, 60);
             this.StopButton.TabIndex = 5;
             this.StopButton.Text = "Stop";
             this.StopButton.UseVisualStyleBackColor = true;
@@ -113,65 +127,77 @@ namespace GoL
             // 
             // fps_lable
             // 
+            this.fps_lable.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.fps_lable.AutoSize = true;
-            this.fps_lable.Location = new System.Drawing.Point(339, 1915);
+            this.fps_lable.Location = new System.Drawing.Point(148, 593);
+            this.fps_lable.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.fps_lable.Name = "fps_lable";
-            this.fps_lable.Size = new System.Drawing.Size(47, 25);
+            this.fps_lable.Size = new System.Drawing.Size(24, 13);
             this.fps_lable.TabIndex = 6;
             this.fps_lable.Text = "fps:";
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(509, 1863);
+            this.label1.Location = new System.Drawing.Point(212, 559);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 7;
             this.label1.Text = "label1";
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(509, 1893);
+            this.label2.Location = new System.Drawing.Point(212, 575);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 8;
             this.label2.Text = "label2";
             // 
             // label3
             // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(509, 1927);
+            this.label3.Location = new System.Drawing.Point(212, 593);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 9;
             this.label3.Text = "label3";
             // 
             // label4
             // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(906, 1863);
+            this.label4.Location = new System.Drawing.Point(212, 608);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(70, 25);
+            this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 10;
             this.label4.Text = "label4";
             // 
             // label5
             // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(906, 1905);
+            this.label5.Location = new System.Drawing.Point(534, 608);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(70, 25);
+            this.label5.Size = new System.Drawing.Size(38, 13);
             this.label5.TabIndex = 11;
-            this.label5.Text = "label5";
+            this.label5.Text = "Speed";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(1894, 1009);
+            this.ClientSize = new System.Drawing.Size(1051, 630);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -184,6 +210,7 @@ namespace GoL
             this.Controls.Add(this.lblHelloWorld);
             this.Controls.Add(this.btnClickThis);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
